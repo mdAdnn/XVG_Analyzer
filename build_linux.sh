9ce4c8a279813a3c8c63869427362131d56bf900
Adnan Mohammed Naeem Mohammed <admo88720@hbku.edu.qa>
2025-10-15 23:07:33 +0300

    Change build script to use xvg_analyzer.py

--- build_linux.sh
+++ build_linux.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "=== Building xvg_plotter for Linux ==="
-pyinstaller --noconsole --onefile xvg_plotter.py
+pyinstaller --noconsole --onefile xvg_analyzer.py
 echo "Build complete! Binary in dist/"
